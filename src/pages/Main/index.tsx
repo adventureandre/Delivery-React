09375667807a469a10b6d8c4ce5aeed781f77748
Andre Luiz <adventureandre@hotmail.com>
2023-02-19 21:43:32 -0300

    criando menu com responsividade

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,17 @@
 import { Container } from './styles'
 import { Sidebar } from "../../components/Sidebar";
 
+import logoImg from "../../assets/logo.svg"
+import { Outlet } from "react-router-dom";
+
 export default function Main() {
   return (
     <Container>
       <Sidebar/>
+      <section>
+        <img src={logoImg} alt="" />
+        <Outlet/>
+      </section>
     </Container>
   )
 }
