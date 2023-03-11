7cd4d259bed62d38682c2205d67e16d235614093
Andre Luiz <adventureandre@hotmail.com>
2023-03-11 17:37:06 -0300

    page creation, creation of the snacks component

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,3 +1,35 @@
-export default function Burgers(){
-  return <h1>Burgers</h1>
+import { Head } from "../../../components/Head";
+import { SnackTitle } from "../../../components/SnackTitle";
+import { Snacks } from "../../../components/Snacks";
+
+export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: "burger",
+      name: "Mega",
+      description:
+        "O artesanal tamanho família recheado com três carnes suculentas, queijo e Bacon.",
+      price: 25.50,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: "burger",
+      name: "Extra Bacon",
+      description:
+        "Criado para os amantes de bacon, possui em todas as suas camadas de bacon bem assado e ainda queijo e carne.",
+      price: 23.50,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ];
+
+  return (
+    <>
+      <Head title="Hambúrgueres" description="Nossos melhores Burguers" />
+      <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={data}></Snacks>
+    </>
+  );
+
 }
